c4ffeee4fd8a72ccc9dcc1beed3364a81b9f8a7c
Kayode <kayodeadekanye@gmail.com>
2018-02-27 15:42:53 +0100

    Created new STS project

---
+++ src/main/java/com/devsopbuddy/DevsopbuddyApplication.java
@@ -0,0 +1,12 @@
+package com.devsopbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsopbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsopbuddyApplication.class, args);
+	}
+}
